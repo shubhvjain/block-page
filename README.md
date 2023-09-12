b37d2145bce6f3d17baf003ea6b5a5f728b56efb
shubh <shubhvjain@gmail.com>
2023-09-12 21:35:18 +0200

    readme and doc structure

---
+++ README.md
@@ -0,0 +1,16 @@
+# Introduction 
+
+This package enables users to create and interact with non linear documents while still retaining the format and simplicity of a linear text file. 
+
+See the project [page](/docs/index.html) to know more and the motivation behind it. 
+
+# Installation 
+
+NPM : ` npm install block-page`
+
+# Usage 
+
+3 main functionalities :
+- `encode`
+- `decode`
+- `action`
--- src/actions.js
+++ src/actions.js
@@ -1,6 +1,18 @@
 // code to perform actions on a document object and  maintain its validity
 
+/**
+ * valid actions 
+ * get blank document 
+ * add a new block in a document
+ * relate one block with another 
+ * edit block : modify text, modify edges
+ * delete block 
+ * add a new block and relate it to another block at the same time
+ * validate documentObject
+ */
+
+
 
 module.exports = {
-
+  
 }
