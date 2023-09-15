ba0727dc4fe7c57b4ab69208f8c2e11e925e9aaf
shubh <shubhvjain@gmail.com>
2023-09-15 17:33:47 +0200

    testing

--- test/action-test.js
+++ test/action-test.js
@@ -10,7 +10,16 @@ let testDelete  = async(opt) =>{
   await u.saveFile(docObj1,opt.outputFile,true)
 }
 
-tests = [
+
+let testEdit  = async(opt) =>{
+  docObj = await u.readFile(opt.fileName,true)
+  //console.log(docObj)
+  // delete independent block
+  docObj1 = a.doEditBlock(docObj,opt.blockToEdit,opt.change)
+  await u.saveFile(docObj1,opt.outputFile,true)
+}
+
+tests1 = [
   {
     "fileName":"./output/5.json",
     "blockToDelete":"indep",
@@ -20,13 +29,38 @@ tests = [
     "fileName":"./output/5.json",
     "blockToDelete":"indep-1",
     "outputFile":"./output/5-2.json"
+  },
+  {
+    "fileName":"./output/6.json",
+    "blockToDelete":"edit-1",
+    "outputFile":"./output/6-2.json"
+  }
+]
+
+tests2 = [
+  {
+    "fileName":"./output/6.json",
+    "blockToEdit":"edit 1",
+    "outputFile":"./output/6-1.json",
+    "change":{"text":"This is the new text"}
+  },
+  {
+    "fileName":"./output/6.json",
+    "blockToEdit":"edit 2",
+    "outputFile":"./output/6-2.json",
+    "change":{"title":"This is the new title"}
+  },
+  {
+    "fileName":"./output/6.json",
+    "blockToEdit":"edit 3",
+    "outputFile":"./output/6-3.json",
+    "change":{"text":"This is the new text","title":"New title"}
   }
 ]
 
 const main = async ()=>{
-  tests.map(t=>{
-    testDelete(t)
-  })
+  // tests1.map(t=>{testDelete(t)})
+  tests2.map(t=>{testEdit(t)})
 }
 
 main()
